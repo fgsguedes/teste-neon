003f1a47afabf462574db866bbeb5c35e1fccba1
Filipe Guedes <fgs.guedes@gmail.com>
2017-01-23 02:22:10 -0200

    Navigation back from SendMoneyActivity to MainActivity

--- app/src/main/java/com/fgsguedes/testeneon/ui/activity/SendMoneyActivity.java
+++ app/src/main/java/com/fgsguedes/testeneon/ui/activity/SendMoneyActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
@@ -90,6 +92,14 @@ public class SendMoneyActivity extends AppCompatActivity implements SendMoneyCon
   }
 
   private void setUpUi() {
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    ActionBar supportActionBar = getSupportActionBar();
+    if (supportActionBar != null) {
+      supportActionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
     linearLayoutManager = new LinearLayoutManager(this);
     contactsAdapter = new ContactsAdapter(this);
 
