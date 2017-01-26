0e94b82d0ea54ccb27edfc047fc0bc1ab3a67292
Filipe Guedes <fgs.guedes@gmail.com>
2017-01-26 01:53:30 -0200

    TransactionHistoryActivity transaction order and mapping test

--- app/src/androidTest/java/com/fgsguedes/testeneon/ui/activity/TransactionHistoryActivityTest.java
+++ app/src/androidTest/java/com/fgsguedes/testeneon/ui/activity/TransactionHistoryActivityTest.java
@@ -1,12 +1,15 @@
 package com.fgsguedes.testeneon.ui.activity;
 
 import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.dannyroa.espresso_samples.recyclerview.RecyclerViewMatcher;
 import com.fgsguedes.testeneon.App;
+import com.fgsguedes.testeneon.R;
 import com.fgsguedes.testeneon.di.ApplicationComponent;
 import com.fgsguedes.testeneon.di.DaggerApplicationComponent;
 import com.fgsguedes.testeneon.di.module.AndroidModule;
@@ -19,12 +22,18 @@ import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okio.BufferedSource;
 import okio.Okio;
 import okio.Source;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -66,8 +75,25 @@ public class TransactionHistoryActivityTest {
 
     activityRule.launchActivity(new Intent());
 
+    Locale locale = InstrumentationRegistry.getTargetContext()
+        .getResources().getConfiguration().locale;
+
+    onView(withRecyclerView(R.id.recycler_view_transactions).atPosition(0))
+        .check(matches(hasDescendant(withText("Filipe Guedes 2"))))
+        .check(matches(hasDescendant(withText(String.format(locale, "R$ %.2f", 320.0)))));
+
+    onView(withRecyclerView(R.id.recycler_view_transactions).atPosition(1))
+        .check(matches(hasDescendant(withText("Filipe Guedes 3"))))
+        .check(matches(hasDescendant(withText(String.format(locale, "R$ %.2f", 75.0)))));
+
+    onView(withRecyclerView(R.id.recycler_view_transactions).atPosition(2))
+        .check(matches(hasDescendant(withText("Filipe Guedes 1"))))
+        .check(matches(hasDescendant(withText(String.format(locale, "R$ %.2f", 22.0)))));
 
     server.shutdown();
   }
 
+  private RecyclerViewMatcher withRecyclerView(@IdRes int viewId) {
+    return new RecyclerViewMatcher(viewId);
+  }
 }
